78d20bf8cf43371c49d269fd78eb32e5541b7b80
kazuki komatsu <spitzspitzspitz14@icloud.com>
2021-03-16 17:56:31 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <button @click = "serch">検索</button>
+    <p>検索結果:{{pokemon}}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data(){
+    return {
+      pokemon:""
+    }
+  },
+  methods:{
+    async serch(){
+      let poke = await axios.get(`https://pokeapi.co/api/v2/type/4/`)
+      console.log(poke);
+      // this.pokemon= poke;
+    }
+  }
+}
+</script>
